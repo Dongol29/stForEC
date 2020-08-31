8bc83901f48cacfe0876e4c3b83edc7c764587a5
dongol cho <dongol29@kaist.ac.kr>
2020-08-31 19:32:29 +0900

    initial xcode project commit

---
+++ stForEC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  stForEC
+//
+//  Created by dongol cho on 2020/08/31.
+//  Copyright © 2020 dongol cho. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

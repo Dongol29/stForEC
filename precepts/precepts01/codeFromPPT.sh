44352e1fcf09f5f99f7ee3a8f47c56849b309fba
dongol cho <dongol29@kaist.ac.kr>
2020-09-01 16:21:12 +0900

    upload precepts01

---
+++ precepts/precepts01/codeFromPPT.sh
@@ -0,0 +1,9 @@
+#URL: http://www.ndsl.kaist.edu/ee209/precepts/01/src/
+#Download source code and compile
+wget www.ndsl.kaist.edu/ee209/precepts/01/src/circle.c
+gcc209 circle.c –o circle
+#Download the binary for base and guess
+wget www.ndsl.kaist.edu/ee209/precepts/01/src/base
+chmod u+x base
+wget www.ndsl.kaist.edu/ee209/precepts/01/src/guess
+chmod u+x guess

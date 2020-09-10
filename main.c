3372d372bcca78e51731692657a133f6906a30b7
dongol cho <dongol29@kaist.ac.kr>
2020-09-10 17:18:50 +0900

    DFA first try

--- main.c
+++ main.c
@@ -1,15 +1,7 @@
-//
-//  main.c
-//  stForEC
-//
-//  Created by dongol cho on 2020/08/31.
-//  Copyright © 2020 dongol cho. All rights reserved.
-//
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("안녕하세요 테스트 브랜치입니다!\n");
-    return 0;
+void main()
+{
+    printf("hello\nworld\n");
 }
---
+++ wc209.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+enum DFAstate {IN,OUT,BAR_1,COMMENT,BAR_3}
+
+int main()
+{
+    int line_count=1,word_count=0,char_count=0,c,comment_line; //line_count는 1로 설정해둠
+    enum DFAstate state=OUT;
+    while((c=getchar())!=EOF){
+        switch(state){
+            case OUT:
+                if(c=='/') {state=BAR_1; char_count++; word_count++;}
+                else if(!isspace(c)&&c!='/') {state=IN; char_count++; word_count++;}
+                else if(isspace(c)&&c!='\n') char_count++;
+                else if(c=='\n') {char_count++; line_count++;}
+                break;
+            case IN:
+                if(c=='/') {state=BAR_1; char_count++;}
+                else if(!isspace(c)&&c!='/') char_count++;
+                else if(isspace(c)&&c!='\n') {state=OUT; char_count++;}
+                else if(c=='\n') {state=OUT; char_count++; line_count++;}
+                break;
+            case BAR_1:
+                if(c=='/') char_count++;
+                else if(c=='\n') {state=IN; char_count++; line_count++;}
+                else if(c!='\n'&&c!='*') {state=IN; char_count++;}
+                else if(c=='*') {state=COMMENT; char_count--; comment_line=line_count;} //COMMENT상태 line 번호 저장
+                break;
+            case COMMENT:
+                if(c=='\n') {char_count++; line_count++;}
+                else if(c!='\n'&&c!='*') ;
+                else if(c=='*') state=BAR_3;
+                break;
+            case BAR_3:
+                if(c=='\n') {state=COMMENT; char_count++; line_count++;}   //'\n'같은게 */에(COMMENT가 되는지) 영향 주나??
+                else if(c!='\n'&&c!='*') state=COMMENT;
+                else if(c=='*') ;
+                else if(c=='/') {state=OUT; char_count++;}
+                break;
+        }
+    }
+    if(char_count==0) line_count=0; //빈 파일-->line수=0
+
+    if(state==COMMENT||state==BAR_3) {
+        pritnf("Error: line %d: unterminated comment\n",comment_line);
+        return EXIT_FAILURE;}
+    else {
+        printf("%d %d %d\n",char_count,word_count,line_count);
+        return EXIT_SUCCESS;}  
+}

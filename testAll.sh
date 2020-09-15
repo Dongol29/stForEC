36c898d45ea202941df2080f814d2e70004ef758
CHO DONG OL <dongol.cho@gmail.com>
2020-09-15 05:26:14 +0000

    move test file to eelab5

---
+++ testAll.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+echo "./samplewc209 <test0.txt"
+./samplewc209 <test0.txt
+echo "./samplewc209 <test12.txt"
+./samplewc209 <test12.txt
+echo "./samplewc209 <test19.txt"
+./samplewc209 <test19.txt
+echo "./samplewc209 <test20.txt"
+./samplewc209 <test20.txt
+echo "./samplewc209 <test4.txt"
+./samplewc209 <test4.txt
+echo "./samplewc209 <test7.txt"
+./samplewc209 <test7.txt
+echo "./samplewc209 <test9.txt"
+./samplewc209 <test9.txt
+echo "./samplewc209 <test16.txt"
+./samplewc209 <test1.txt
+echo "./samplewc209 <test1.txt"
+./samplewc209 <test16.txt
+echo "./samplewc209 <test2.txt"
+./samplewc209 <test2.txt
+echo "./samplewc209 <test3.txt"
+./samplewc209 <test3.txt
+echo "./samplewc209 <test5.txt"
+./samplewc209 <test5.txt
+echo "./samplewc209 <test8.txt"
+./samplewc209 <test8.txt
+echo "./samplewc209 <test30.txt"
+./samplewc209 <test30.txt
+echo "./samplewc209 <test31.txt"
+./samplewc209 <test31.txt
+echo "./samplewc209 <test32.txt"
+./samplewc209 <test32.txt
+echo "./samplewc209 <test33.txt"
+./samplewc209 <test33.txt
+echo "./samplewc209 <test34.txt"
+./samplewc209 <test34.txt
+echo "./samplewc209 <test35.txt"
+./samplewc209 <test35.txt
+echo "./samplewc209 <test36.txt"
+./samplewc209 <test36.txt
+echo "./samplewc209 <test37.txt"
+./samplewc209 <test37.txt
+echo "./samplewc209 <test38.txt"
+./samplewc209 <test38.txt
+echo "./samplewc209 <test39.txt"
+./samplewc209 <test39.txt
+echo "./samplewc209 <test40.txt"
+./samplewc209 <test40.txt
+echo "./samplewc209 <test41.txt"
+./samplewc209 <test41.txt

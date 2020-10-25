98979eb35a2e932f1745575f371b06ac07af0611
dongol cho <dongol29@kaist.ac.kr>
2020-10-25 22:25:52 +0900

    maybe?

--- 20190617_assign3/customer_manager2.c
+++ 20190617_assign3/customer_manager2.c
@@ -130,11 +130,13 @@ RegisterCustomer(DB_T d, const char *id,
       }}
   } 
   /*중복된 거 없다는 게 확인됨*/
+  p=(USERINFO *)calloc(1,sizeof(USERINFO));
+
   printf("3\n");
   p->id=strdup(id);
   //p->id=(char *)malloc(strlen(id)+1); 
   //printf("haha\n");
-  strcpy(p->id,id);
+  //strcpy(p->id,id);
   printf("4\n");
   p->name=strdup(name);
   p->purchase=purchase;

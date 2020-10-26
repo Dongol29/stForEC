48689597181abe8dd8301c3c2e846208b8578ec1
dongol cho <dongol29@kaist.ac.kr>
2020-10-26 21:00:13 +0900

    loop

--- 20190617_assign3/customer_manager2.c
+++ 20190617_assign3/customer_manager2.c
@@ -191,7 +191,6 @@ UnregisterCustomerByID(DB_T d, const char *id)
   USERINFO *pf0=p;
 
   for(;p->next!=NULL;p=p->next){
-    printf("a\n");
     if(p->next->id_hash==h1&&strcmp(id,p->next->id)==0){
       count++;
       p->next=p->next->next;

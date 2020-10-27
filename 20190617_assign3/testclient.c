e2a586e3b45ca208a247e208c12fea8afe4aabc1
dongol cho <dongol29@kaist.ac.kr>
2020-10-27 14:51:44 +0900

    fd

--- 20190617_assign3/testclient.c
+++ 20190617_assign3/testclient.c
@@ -481,7 +481,7 @@ PerformanceTest(int num) {
 	/* start timer */
 	gettimeofday(&start, NULL);
 	/* run test */
-	for (i = 0; i < num-2; i++) { //-1
+	for (i = 0; i < num; i++) { //-1
 		sprintf(name, "name%d", i);
 		assert(UnregisterCustomerByName(d, name) == 0); //name->id
 		printf("%d",i+1);

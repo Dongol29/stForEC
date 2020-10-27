e9960c8eb1dc31d53688d20752d3595029820b79
dongol cho <dongol29@kaist.ac.kr>
2020-10-27 12:20:02 +0900

    hola

--- 20190617_assign3/customer_manager2.c
+++ 20190617_assign3/customer_manager2.c
@@ -187,13 +187,24 @@ UnregisterCustomerByID(DB_T d, const char *id)
   int name_hash,n=0;
   USERINFO *p,*q;
 
-  p=(USERINFO *)malloc(sizeof(USERINFO));
-  p->next=d->first;
-  USERINFO *pf0=p;
   
   while(d->first&&d->first->id_hash==h1&&strcmp(d->first->id,id)==0){
     d->first=d->first->next;
+
+    name_hash=d->id_bucket[h1&(d->curBuckSize-1)]->name_hash;
+    free(d->id_bucket[h1&(d->curBuckSize-1)]->id);   
+    d->id_bucket[h1&(d->curBuckSize-1)]=
+                    d->id_bucket[h1&(d->curBuckSize-1)]->id_next;
+
+    free(d->name_bucket[name_hash&(d->curBuckSize-1)]->name);  
+    d->name_bucket[name_hash&(d->curBuckSize-1)]=
+                    d->name_bucket[name_hash&(d->curBuckSize-1)]->name_next;             
+
   }
+
+  p=(USERINFO *)malloc(sizeof(USERINFO));
+  p->next=d->first;
+  USERINFO *pf0=p;
   
   for(;p->next!=NULL;p=p->next){
     printf("ha\n");
@@ -266,6 +277,16 @@ UnregisterCustomerByName(DB_T d, const char *name)
 
   while(d->first&&d->first->name_hash==h1&&strcmp(d->first->name,name)==0){
     d->first=d->first->next;
+
+    id_hash=d->name_bucket[h1&(d->curBuckSize-1)]->id_hash;
+    free(d->name_bucket[h1&(d->curBuckSize-1)]->name);   
+    d->name_bucket[h1&(d->curBuckSize-1)]=
+                    d->name_bucket[h1&(d->curBuckSize-1)]->name_next;
+                    
+    free(d->id_bucket[id_hash&(d->curBuckSize-1)]->id);  
+    d->id_bucket[id_hash&(d->curBuckSize-1)]=
+                    d->id_bucket[id_hash&(d->curBuckSize-1)]->id_next;             
+
   }
   p=(USERINFO *)malloc(sizeof(USERINFO));
   p->next=d->first;

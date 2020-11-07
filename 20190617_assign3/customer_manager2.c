8a0d438d4505b937c7ecf16a306e13bffbfdc9b9
dongol cho <dongol29@kaist.ac.kr>
2020-11-07 15:07:48 +0900

    hial

--- 20190617_assign3/customer_manager2.c
+++ 20190617_assign3/customer_manager2.c
@@ -355,7 +355,7 @@ UnregisterCustomerByName(DB_T d, const char *name)
   USERINFO *pf2=p;
   
   for(;p->id_next!=NULL;p=p->id_next){
-    if(p->id_next->id!=NULL&&p->id_next->id_hash!=NULL);
+    if(p->id_next->id!=NULL&&p->id_next->id_hash);
     if(id_hash==p->id_next->id_hash&&strcmp(id,p->id_next->id)==0){
       count++;
       free(p->id_next->id);

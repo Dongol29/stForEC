de45f57a2259265cb1b5893bf6dc5e8461d734b9
dongol cho <dongol29@kaist.ac.kr>
2020-11-21 16:25:57 +0900

    testAll.sh

---
+++ 20190617_assign4/testAll.sh
@@ -0,0 +1,14 @@
+echo "dc < values1.txt"
+dc < values1.txt
+echo "dc < values2.txt"
+dc < values2.txt
+echo "dc < values3.txt"
+dc < values3.txt
+echo "dc < values4.txt"
+dc < values4.txt
+echo "dc < values5.txt"
+dc < values5.txt
+echo "dc < values6.txt"
+dc < values6.txt
+echo "dc < values7.txt"
+dc < values7.txt

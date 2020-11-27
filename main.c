49257416657818d40a706b1ef7b8ba73ce7d555c
dongol cho <dongol29@kaist.ac.kr>
2020-11-27 19:32:00 +0900

    hola

--- main.c
+++ main.c
@@ -4,20 +4,8 @@
 #include <assert.h>
 
 
-int prime(int a) //a>=2
-{
-	int i,n;
-	unsigned int count=0; //a이하의 소수는 무조건 존재함
-	for(n=a;n>=2;n--){
-		for(i=2;i<n;i++){
-			if(n%i==0) break;
-			if(i==n-1)  return n;
-		}
-	    continue;
-	}
-}
-
 void main()
 {
-    printf("%d\n",prime(96));   
+    int a=1;
+    if(a--) printf("hola\n");
 }

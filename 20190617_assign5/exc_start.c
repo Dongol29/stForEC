b69ddf0a1b0b8bd546663bc84713963f69387e80
dongol cho <dongol29@kaist.ac.kr>
2020-12-20 22:38:15 +0900

    562

--- 20190617_assign5/exc_start.c
+++ 20190617_assign5/exc_start.c
@@ -549,7 +549,7 @@ int exc2_Line(char ***cmds,int num_pipe)
             close(p[i-1][0]); 
             close(p[i-1][1]);
          }
-         if(i<num_pipe){
+         if(i!=num_pipe){
             dup2(p[i][1],1);
             /*
             out=open("/dev/tty",O_RDONLY | O_TRUNC | O_CREAT, 0600);
@@ -559,7 +559,7 @@ int exc2_Line(char ***cmds,int num_pipe)
             //close(p[i][1]);
          }
          
-         else if(i>num_pipe-1){
+         else{
             out=open("/dev/tty",O_RDONLY | O_TRUNC | O_CREAT, 0600);
             dup2(out,1);
             close(out);
